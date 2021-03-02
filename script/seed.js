8daad77aaf86f9a2b09f9a112788de8e3aae0a98
Evan-Fisher <evankfisher@gmail.com>
2021-03-02 10:54:07 -0600

    product model

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Product} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -10,6 +10,15 @@ async function seed() {
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
+  ])
+
+  const products = await Promise.all([
+    Product.create({
+      name: 'tv',
+      description: 'a tv the best',
+      rating: 4,
+      price: 129.99
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
