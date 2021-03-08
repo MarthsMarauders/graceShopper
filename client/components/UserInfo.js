498846953d958cf09723d7526f60acf61cc1a7b3
MBusto <mikebusto4@gmail.com>
2021-03-08 13:46:49 -0500

    fixed bug, users route set up

--- client/components/UserInfo.js
+++ client/components/UserInfo.js
@@ -5,28 +5,30 @@ import {Link} from 'react-router-dom'
 
 class UserInfo extends React.Component {
   componentDidMount() {
+    console.log('MOUNTED!!!!')
     this.props.fetchAllUsers()
   }
 
   render() {
     const {users} = this.props
-    console.log('THIS IS USERS STATE--->', users)
-    return (
+    console.log('THIS IS USERS--->', users)
+    return users ? (
       <div>
         {users.map(user => (
           <div key={user.id}>
             <h3>UserId: {user.id}</h3>
             <h1>UserEmail: {user.email}</h1>
-            <h3>{user.isAdmin ? 'Admin' : 'NotAdmin'}</h3>
+            <h3>Admin: {user.isAdmin ? 'Admin' : 'NotAdmin'}</h3>
           </div>
         ))}
       </div>
-    )
+    ) : null
   }
 }
 
 const mapStateToProps = state => {
   return {
+    ...state,
     users: state.user.users
   }
 }
--- client/store/user.js
+++ client/store/user.js
@@ -1,4 +1,98 @@
-/* eslint-disable no-case-declarations */
+// /* eslint-disable no-case-declarations */
+// import axios from 'axios'
+// import history from '../history'
+
+// /**
+//  * ACTION TYPES
+//  */
+// const GET_USER = 'GET_USER'
+// const GET_ALL_USERS = 'GET_ALL_USERS'
+// const REMOVE_USER = 'REMOVE_USER'
+
+// /**
+//  * INITIAL STATE
+//  */
+// const initialState = {
+//   users: [],
+//   user: {}
+// }
+
+// /**
+//  * ACTION CREATORS
+//  */
+// const getUser = user => ({type: GET_USER, user})
+// const removeUser = () => ({type: REMOVE_USER})
+// const getAllUsers = users => ({
+//   type: GET_ALL_USERS,
+//   users
+// })
+
+// /**
+//  * THUNK CREATORS
+//  */
+// export const me = () => async dispatch => {
+//   try {
+//     const res = await axios.get('/auth/me')
+//     dispatch(getUser(res.data || {}))
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
+// export const auth = (email, password, method) => async dispatch => {
+//   let res
+//   try {
+//     res = await axios.post(`/auth/${method}`, {email, password})
+//   } catch (authError) {
+//     return dispatch(getUser({error: authError}))
+//   }
+//   try {
+//     dispatch(getUser(res.data))
+//     history.push('/home')
+//   } catch (dispatchOrHistoryErr) {
+//     console.error(dispatchOrHistoryErr)
+//   }
+// }
+
+// export const fetchAllUsers = () => {
+//   return async dispatch => {
+//     try {
+//       const {data} = await axios.get('/api/users')
+//       dispatch(getAllUsers(data))
+//     } catch (error) {
+//       console.log(error, 'trouble fetching')
+//     }
+//   }
+// }
+
+// export const logout = () => async dispatch => {
+//   try {
+//     await axios.post('/auth/logout')
+//     dispatch(removeUser())
+//     history.push('/login')
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
+// /**
+//  * REDUCER
+//  */
+// export default function(state = initialState, action) {
+//   switch (action.type) {
+//     // case GET_USER:
+//     //   return action.user
+//     case REMOVE_USER:
+//       return {}
+//     case GET_ALL_USERS:
+//       return {...state, users: action.users}
+//     case GET_USER:
+//       return action.user
+//     default:
+//       return state
+//   }
+// }
+
 import axios from 'axios'
 import history from '../history'
 
@@ -6,8 +100,8 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_USER = 'GET_USER'
-const GET_ALL_USERS = 'GET_ALL_USERS'
 const REMOVE_USER = 'REMOVE_USER'
+const GET_ALL_USERS = 'GET_ALL_USERS'
 
 /**
  * INITIAL STATE
@@ -21,11 +115,8 @@ const initialState = {
  * ACTION CREATORS
  */
 const getUser = user => ({type: GET_USER, user})
+const getUsers = users => ({type: GET_ALL_USERS, users})
 const removeUser = () => ({type: REMOVE_USER})
-const getAllUsers = users => ({
-  type: GET_ALL_USERS,
-  users
-})
 
 /**
  * THUNK CREATORS
@@ -34,6 +125,7 @@ export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me')
     dispatch(getUser(res.data || {}))
+    // history.push('/home')
   } catch (err) {
     console.error(err)
   }
@@ -46,6 +138,7 @@ export const auth = (email, password, method) => async dispatch => {
   } catch (authError) {
     return dispatch(getUser({error: authError}))
   }
+
   try {
     dispatch(getUser(res.data))
     history.push('/home')
@@ -58,7 +151,7 @@ export const fetchAllUsers = () => {
   return async dispatch => {
     try {
       const {data} = await axios.get('/api/users')
-      dispatch(getAllUsers(data))
+      dispatch(getUsers(data))
     } catch (error) {
       console.log(error, 'trouble fetching')
     }
@@ -80,20 +173,12 @@ export const logout = () => async dispatch => {
  */
 export default function(state = initialState, action) {
   switch (action.type) {
-    // case GET_USER:
-    //   return action.user
-    // case REMOVE_USER:
-    //   return defaultUser
-    // case GET_ALL_USERS
-    //   return action.products
     case GET_ALL_USERS:
       return {...state, users: action.users}
     case GET_USER:
-      return {...state, user: action.user}
+      return action.user
     case REMOVE_USER:
-      const userId = action.user.id
-      const newUsers = state.users.filter(user => user.id !== userId)
-      return {...state, products: newUsers}
+      return {}
     default:
       return state
   }
