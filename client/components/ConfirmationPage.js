2e12bb0d270639b70da83bf9d3f4e7cf8da58b6f
MBusto <mikebusto4@gmail.com>
2021-03-09 21:36:08 -0500

    Now final Css touches

--- client/components/ConfirmationPage.js
+++ client/components/ConfirmationPage.js
@@ -3,13 +3,18 @@ import {Link} from 'react-router-dom'
 
 const ConfirmationPage = () => {
   return (
-    <div>
+    <div className="confirmation-page">
       <h1>Thanks for your purchase!</h1>
-      <img src="https://i.pinimg.com/564x/ee/a9/c6/eea9c6878701d9d453480dfc2ec40f5b.jpg" />
+      <img
+        className="confirmation-img"
+        src="https://i.pinimg.com/564x/ee/a9/c6/eea9c6878701d9d453480dfc2ec40f5b.jpg"
+      />
       <br />
       <br />
       <Link to="/products">
-        <button type="button">Continue Shopping</button>
+        <button type="button" className="btn btn-success btn-lg">
+          Continue Shopping!!!
+        </button>
       </Link>
     </div>
   )
